5f358108922940705a383bba6a01464cdabbbd2d
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-07-31 00:02:09 +0300

    Refactor TodoApp: Introduced GoalInput component for goal entry, updated GoalItem for deletion functionality, and modified state management in HomeScreen.

--- Section 2/TodoApp2/app/_layout.tsx
+++ Section 2/TodoApp2/app/_layout.tsx
@@ -1,53 +1,48 @@
 import { useState } from "react";
-import {
-  Button,
-  FlatList,
-  StyleSheet,
-  TextInput,
-  View
-} from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 // Components
+import GoalInput from "@/components/GoalInput";
 import GoalItem from "@/components/GoalItem";
-
 export interface ICourseGoal {
-  enteredGoal: string;
+  enteredText: string;
   id: string;
 }
 
 export default function HomeScreen() {
-  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState<ICourseGoal[]>([]);
-  function goalInputHandler(enteredText: string) {
-    setEnteredGoal(enteredText);
-  }
 
-  function addGoalHandler() {
-    if (!enteredGoal) {
-      alert("Cannot be empty");
-    }
+  function addGoalHandler(enteredText: string) {
+    console.log(enteredText);
+
     setCourseGoals((prevGoals) => [
       ...prevGoals,
-      { enteredGoal, id: Math.random().toString() },
+      { enteredText, id: Math.random().toString() },
     ]);
+  }
+
+  function deleteGoalHandler(id: string) {
+    console.log("DELETE");
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your course goal!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList
           data={courseGoals}
-          renderItem={(itemData) => <GoalItem goal={itemData.item} />}
+          renderItem={(itemData) => (
+            <GoalItem
+              goal={itemData.item}
+              onDeleteItem={deleteGoalHandler}
+              goalId={itemData.item.id}
+            />
+          )}
           keyExtractor={(item, index) => {
-            return item.id
+            return item.id;
           }}
           alwaysBounceVertical={false}
         />
@@ -62,32 +57,7 @@ const styles = StyleSheet.create({
     padding: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
-  },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
--- Section 2/TodoApp2/components/GoalItem.tsx
+++ Section 2/TodoApp2/components/GoalItem.tsx
@@ -1,27 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 import { ICourseGoal } from "@/app/_layout";
 
 interface GoalItemProps {
   goal: ICourseGoal;
+  goalId: string;
+  onDeleteItem: (id: string) => void;
 }
 
-export default function GoalItem({ goal }: GoalItemProps) {
-    return (
-        <View style={styles.goalItem}>
-            <Text style={styles.goalText}>{goal.enteredGoal}</Text>
-        </View>
-    );
+export default function GoalItem({
+  goal,
+  onDeleteItem,
+  goalId,
+}: GoalItemProps) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#5e4acc" }}
+        onPress={() => onDeleteItem(goal.id)}
+        style={({pressed}) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{goal.enteredText}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    goalItem: {
-        margin: 8,
-        padding: 8,
-        borderRadius: 6,
-        backgroundColor: "#5e0acc",
-    },
-    goalText: {
-        color: "white",
-    }
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5
+  },
+  goalText: {
+    padding: 8,
+    color: "white",
+  },
 });
