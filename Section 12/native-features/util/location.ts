af656fec73d7315fc6388c558833fbd76b590838
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-08-03 15:31:17 +0300

    Update dotenv dependency: Upgrade dotenv to version 17.2.1 in package.json and package-lock.json, and refactor location.ts to use environment variable for Google API key.

--- Section 12/native-features/util/location.ts
+++ Section 12/native-features/util/location.ts
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAMk8wm642yi2GoV6unVkKY-YC8cI-InAs";
+const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
 
 function getMapPreview(lat: number, lng: number) {
     const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
